7805354aa3f6c3cb0a58f9e7fd0005b3eb2a7406
robertmulvey <robert.mulvey@onefact.net>
2016-12-08 13:54:42 -0500

    job #8906 I introduced a utility operation , Export OAL Instance to the Clipboard (ExportOALInstance.java). This appears under the Bridgepoint Utilites menu. It works just like the existing copy (copies model elements to the clipboard), but it only copies the OAL instances. If there are no OAL instances then you end up with file header info on the clipboard with nothing else (no instances). Note that this explicitly does NOT parse the data. To use this, it is the callers job to have parsed.

---
+++ src/org.xtuml.bp.utilities/src/org/xtuml/bp/utilities/actions/ExportOALInstances.java
@@ -0,0 +1,82 @@
+package org.xtuml.bp.utilities.actions;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.ui.IActionDelegate;
+import org.xtuml.bp.core.CorePlugin;
+import org.xtuml.bp.core.common.NonRootModelElement;
+import org.xtuml.bp.core.ui.CopyCutAction;
+import org.xtuml.bp.io.core.CoreExport;
+
+public class ExportOALInstances implements IActionDelegate {
+	private ISelection selection = null;
+
+	@Override
+	public void run(IAction action) {
+		List<NonRootModelElement> elementsToExport = new ArrayList<NonRootModelElement>();
+		IStructuredSelection ss = (IStructuredSelection) this.selection;
+		for (Iterator<?> i = ss.iterator(); i.hasNext();) {
+			NonRootModelElement nrme = ((NonRootModelElement) i.next());
+			elementsToExport.add(nrme);
+		}
+		
+		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+		CoreExport exporter = (CoreExport) org.xtuml.bp.core.CorePlugin
+				.getStreamExportFactory().create(
+						outStream,
+						elementsToExport
+								.toArray(new NonRootModelElement[elementsToExport
+										.size()]), true, true);
+
+			exporter.setExportOAL(CoreExport.YES);
+			exporter.setExportGraphics(CoreExport.NO);
+			exporter.setExportOnlyOAL(true);
+
+//				// Perform a parse-all to assure the model is up to date
+//				exporter.parseAllForExport(elementsToExport
+//						.toArray(new NonRootModelElement[elementsToExport.size()]),
+//						monitor);
+
+			try {
+				exporter.run(new NullProgressMonitor());
+			} catch (InvocationTargetException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			Clipboard cb = CorePlugin.getSystemClipboard();
+			cb.clearContents();
+			try {
+				outStream.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			String streamContents = new String(outStream.toByteArray());
+			streamContents = streamContents.replaceFirst("\n", "\n" + CopyCutAction.GENERIC_PACKAGE_HEADER + "\n");
+			cb.setContents(new Object[] { streamContents },
+					new Transfer[] { TextTransfer.getInstance() });
+	}
+
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+		this.selection = selection;
+	}
+
+}
